76f31b39c233a62d8b7b286d6a2b9b3ad0925b20
Huynh Van Vinh <vinhhv@live.com>
2024-05-15 14:31:05 +0700

    Day 006 completed - Function and While loop

---
+++ day006/01.loopAndWhile.py
@@ -0,0 +1,15 @@
+"""
+For loop
+for item in list_of_items:
+    Do something to each item
+
+for number in range (a,b):
+    do something
+
+"""
+
+"""
+While loop
+white something_is_true:
+    Do something repeatedly
+"""
