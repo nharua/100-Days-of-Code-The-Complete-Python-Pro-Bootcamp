62b55eb48ff385f7d504b4dbfa222aa86df8d626
Huynh Van Vinh <vinhhv@live.com>
2024-05-16 11:29:11 +0700

    Day 007 completed - Hangman project

---
+++ day007/01.pickaRandomWords.py
@@ -0,0 +1,18 @@
+import random
+
+# Step 1
+
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO:1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+word = random.choice(word_list).lower()
+print(word)
+# TODO:2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guest = input("Guess a letter? ").lower()
+
+# TODO:3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in word:
+    if letter == guest:
+        print("correct")
+    else:
+        print("wrong")
